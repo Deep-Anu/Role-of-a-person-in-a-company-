b2a5ee0439201cb9bbb6d703fbc32c56bfa46868
The-sadhguru <32882887+The-sadhguru@users.noreply.github.com>
2019-02-20 14:17:24 -0500

    Add files via upload

---
+++ dictonary_game.py
@@ -0,0 +1,9 @@
+word  = input('enter a word: ')
+
+dictonary = {'anudeep':'Developer', 'tiru':'CEO','manish':'Developer', 'kiran':'Lead'}
+
+if word in dictonary:
+    print('your entry is',dictonary[word]) 
+else:
+    print('wront entry')    
+
